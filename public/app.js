29556de704489918dbe2e71a9a625bafedf91f14
shofikul <179398ssshofik@gmail.com>
2024-05-21 12:25:46 +0600

    added vercel file

--- public/app.js
+++ public/app.js
@@ -3,16 +3,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
+const http_status_1 = __importDefault(require("http-status"));
+const GlobalErrorHanler_1 = __importDefault(require("./app/middleware/GlobalErrorHanler"));
 const notFound_1 = __importDefault(require("./app/middleware/notFound"));
 const routes_1 = __importDefault(require("./app/routes"));
 const sendResponse_1 = __importDefault(require("./app/utils/sendResponse"));
-const http_status_1 = __importDefault(require("http-status"));
-const GlobalErrorHanler_1 = __importDefault(require("./app/middleware/GlobalErrorHanler"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
+app.use('/api', routes_1.default);
 app.get('/', (req, res) => {
     return (0, sendResponse_1.default)(res, {
         success: true,
@@ -20,7 +21,6 @@ app.get('/', (req, res) => {
         message: 'Welcome to event 360 backend',
     });
 });
-app.use('/api', routes_1.default);
 // not found route
 app.use(notFound_1.default);
 // global error handler
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
-import express from 'express'
 import cors from 'cors'
+import express from 'express'
+import httpStatus from 'http-status'
+import globalErrorHandler from './app/middleware/GlobalErrorHanler'
 import notFound from './app/middleware/notFound'
 import router from './app/routes'
 import sendResponse from './app/utils/sendResponse'
-import httpStatus from 'http-status'
-import globalErrorHandler from './app/middleware/GlobalErrorHanler'
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/api', router)
 
 app.get('/', (req, res) => {
   return sendResponse(res, {
@@ -17,8 +19,6 @@ app.get('/', (req, res) => {
     message: 'Welcome to event 360 backend',
   })
 })
-
-app.use('/api', router)
 
 // not found route
 app.use(notFound)
